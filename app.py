b4e5cf901a9c72dd3078bd5a564ae4420a6ea145
Lai Wei Jie <130772445+moomoolai@users.noreply.github.com>
2025-02-13 08:45:17 +0800

    set up sqlalchemy connection, create data class, create base.html for inheritance

--- app.py
+++ app.py
@@ -2,10 +2,33 @@
 from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy import func
 
 # App 
 app = Flask(__name__)
+Scss(app)
 
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+
+# initialize the app with the extension
+db = SQLAlchemy(app)
+
+
+# Data Class ~ Row of data
+class MyTask(db.Model): 
+
+    # Unique ID
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(100), nullable = False)
+    complete = db.Column(db.Integer)
+    created = db.Column(db.DateTime, default=func.now())
+
+
+
+
+# Routes
 @app.route("/")
 def index():
     return render_template("index.html")
